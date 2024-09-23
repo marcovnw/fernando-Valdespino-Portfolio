e2d90c05e77aad6c91f9fdc7f70c4edde780b1e9
marco <marco.valdespino@nwktc.edu>
2024-09-22 18:56:43 -0600

    initial commit

---
+++ fernando-Portfolio/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+
+export const routes: Routes = [ {path: 'about-me', component: AboutMeComponent}];
